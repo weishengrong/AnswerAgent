a72b256a881ac9c6df5414a0ac02083ec8390d72
qcloud <ubuntu@localhost.localdomain>
2026-05-07 23:44:44 +0800

    添加gitignore,修复线上bug

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ app = FastAPI(title="AnswerAgent", version="2.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[ "http://124.223.93.65", "http://124.223.93.65:3000", "http://127.0.0.1"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
